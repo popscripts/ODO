9b94cdcfd32bdbae9ae1a47ac82771b873992dae
bartoszposzelezny <bartoszposzelezny@gmail.com>
2023-06-29 15:35:09 +0200

    change database schema (controllers)

--- server/src/controllers/buffet.controller.ts
+++ server/src/controllers/buffet.controller.ts
@@ -6,12 +6,14 @@ import { Token } from '../types/auth.type'
 import * as Callback from '../libs/callbacks'
 import { statuses } from '../libs/statuses'
 import { logger } from '../config/logger'
+import { NewOrder, Order } from '../types/buffet.type'
+import { Dishes } from '../libs/dishes'
 
 export const orders = async (request: Request, response: Response) => {
     try {
         const token = request.cookies.JWT
         const tokenData: Token = verifyToken(token, 'accessToken')
-        const orders = await BuffetService.getOrders(tokenData.openDayId)
+        const orders: Order[] = await BuffetService.getOrders(tokenData.openDayId)
         return response.status(200).json({ result: orders, error: 0 })
     } catch (error: any) {
         logger.error(`500 | ${error}`)
@@ -22,8 +24,8 @@ export const orders = async (request: Request, response: Response) => {
 export const userOrders = async (request: Request, response: Response) => {
     try {
         const token: string = request.cookies.JWT
-        const tokenData = verifyToken(token, 'accessToken')
-        const usersOrders = await BuffetService.getUserOrders(tokenData.id)
+        const tokenData: Token = verifyToken(token, 'accessToken')
+        const usersOrders: Order[] = await BuffetService.getUserOrders(tokenData.id)
         return response.status(200).json({ result: usersOrders, error: 0 })
     } catch (error: any) {
         logger.error(`500 | ${error}`)
@@ -37,7 +39,7 @@ export const ordersByStatus = async (request: Request, response: Response) => {
         const token = request.cookies.JWT
         const tokenData: Token = verifyToken(token, 'accessToken')
         const statusId: number = statuses[status]
-        const orders = await BuffetService.getOrdersByStatus(tokenData.openDayId, statusId)
+        const orders: Order[] = await BuffetService.getOrdersByStatus(tokenData.openDayId, statusId)
         return response.status(200).json({ result: orders, error: 0 })
     } catch (error: any) {
         logger.error(`500 | ${error}`)
@@ -47,9 +49,10 @@ export const ordersByStatus = async (request: Request, response: Response) => {
 
 export const placeOrder = async (request: Request, response: Response) => {
     try {
-        const order: string = request.body.order
-        const tokenData = verifyToken(request.cookies.JWT, 'accessToken')
-        await BuffetService.placeOrder(tokenData.openDayId, tokenData.id, order)
+        const { dish, amount, comment }: NewOrder = request.body
+        const { openDayId, id }: Token = verifyToken(request.cookies.JWT, 'accessToken')
+        const dishId: number = Dishes[dish]
+        await BuffetService.placeOrder(openDayId, id, dishId, amount, comment)
         return response.status(201).json(Callback.newOrder)
     } catch (error: any) {
         logger.error(`500 | ${error}`)
