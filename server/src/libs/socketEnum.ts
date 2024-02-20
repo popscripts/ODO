811efc9e9d05a8f7dd05f401c18c998f464e6159
asenath0 <kasiapstrokonska03@gmail.com>
2024-02-20 20:01:53 +0100

    remove server and move client up

--- server/src/libs/socketEnum.ts
+++
@@ -1,4 +0,0 @@
-export enum SocketEnum {
-    EventName = 0,
-    EventData = 1
-}
--- server/src/types/info.type.ts
+++
@@ -1,8 +0,0 @@
-export type Info = {
-    id: number
-    openDayId: number
-    content: string
-    deleted: boolean
-    createdAt: Date
-    updatedAt: Date
-}
